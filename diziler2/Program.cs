276e95f289299f80d6fa9f58b7a3e7d3bf633d72
Keskin6161 <76918333+Keskin6161@users.noreply.github.com>
2021-05-20 13:52:21 +0300

    Add files via upload

---
+++ diziler2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace diziler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = {23,12,4,86,72,3,31,17};
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            Console.WriteLine("**** Sıralı Dizi ****");
+
+            Array.Sort(sayiDizisi);    
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+            Array.Clear(sayiDizisi,2,2); 
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            Array.Resize < int > (ref sayiDizisi,9);
+            sayiDizisi[9] = 99;
+
+        }
+    }
+}
